8e8f1ae99d5011c8dae79460415e5f72fdcb0d86
Arnav Sharma <arnavsharma2711@gmail.com>
2024-05-02 15:54:30 +0530

    Add initial files and configurations

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Candidate Application Platform by <a
+            className="App-link"
+            href="https://www.linkedin.com/in/arnavsharma2711/"
+            target="_blank"
+            rel="noopener noreferrer">
+              Arnav Sharma
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
