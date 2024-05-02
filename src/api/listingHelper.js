a61f75829b5c2eab635983e4957df2702b7da7ae
Arnav Sharma <arnavsharma2711@gmail.com>
2024-05-02 16:49:18 +0530

    Adding Job List API

---
+++ src/api/listingHelper.js
@@ -0,0 +1,8 @@
+import ApiUtils from "../utils/ApiUtils";
+import { JOB_LISTING_API } from "../utils/constants";
+
+function getJobList(limit = 10, offset = 0) {
+    return ApiUtils.request("POST", JOB_LISTING_API, { limit, offset })
+}
+
+export default getJobList;
---
+++ src/utils/constants.js
@@ -0,0 +1,1 @@
+export const JOB_LISTING_API = "https://api.weekday.technology/adhoc/getSampleJdJSON";
