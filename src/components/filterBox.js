d9b79380270556b8ea43a960165109508f51252c
Arnav Sharma <arnavsharma2711@gmail.com>
2024-05-03 06:28:47 +0530

    adding filters options

--- src/components/filterBox.js
+++ src/components/filterBox.js
@@ -1,9 +1,88 @@
 import React from 'react';
+import { FormControl, InputLabel, Select, MenuItem, Checkbox, ListItemText, FormControlLabel, Switch, TextField, Grid, Box, Typography } from '@mui/material';
 
-function FilterBox() {
+function FilterBox({ filter, setFilter, filterOptions }) {
+  const handleFilterChange = (event) => {
+    const { name, value } = event.target;
+    setFilter(prevFilter => ({ ...prevFilter, [name]: Array.isArray(value) ? value : [value] }));
+  };
+
+  const handleSwitchChange = (event) => {
+    const { name, checked } = event.target;
+    setFilter(prevFilter => ({ ...prevFilter, [name]: checked }));
+  };
+
+  const handleNumberInputChange = (event) => {
+    const { name, value } = event.target;
+    setFilter(prevFilter => ({ ...prevFilter, [name]: value === '' ? '' : Number(value) }));
+  };
+
   return (
-    <div> FilterBox </div>
-    
+<Grid container justifyContent="center">
+      <Box sx={{ width: '80%', mt: 3, mb: 3 }}>
+      <Typography variant="h4" align="center" gutterBottom>
+        Filter Options
+      </Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+        <InputLabel id="location-label">Location</InputLabel>
+        <Select
+          labelId="location-label"
+          name="location"
+          multiple
+          value={filter.location}
+          onChange={handleFilterChange}
+          renderValue={(selected) => selected.join(', ')}
+        >
+          {filterOptions.location.map((loc, index) => (
+            <MenuItem key={index} value={loc}>
+              <Checkbox checked={filter.location.indexOf(loc) > -1} />
+              <ListItemText primary={loc} />
+            </MenuItem>
+          ))}
+        </Select>
+        </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              name="minExperience"
+              value={filter.minExperience}
+              onChange={handleNumberInputChange}
+              label="Minimum Experiences"
+              type="number"
+              InputProps={{ inputProps: { min: 0 } }}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              name="minSalary"
+              value={filter.minSalary}
+              onChange={handleNumberInputChange}
+              label="Minimum Salary"
+              type="number"
+              InputProps={{ inputProps: { min: 0 } }}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={filter.isRemote}
+                  onChange={handleSwitchChange}
+                  name="isRemote"
+                  color="primary"
+                />
+              }
+              label="Remote"
+            />
+          </Grid>
+        </Grid>
+
+      </Box>
+    </Grid>
   );
 }
 
--- src/pages/jobList.js
+++ src/pages/jobList.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import JobCard from '../components/jobCard';
-import { Box, CircularProgress, Container, Grid } from '@mui/material';
+import { Box, CircularProgress, Container, Grid, Typography } from '@mui/material';
 import FilterBox from '../components/filterBox';
 import { fetchJobList } from '../store/listing';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
@@ -8,12 +8,37 @@ import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 function JobList() {
   const dispatch = useDispatch();
   const {
-    jobList,
+    jobList, totalCount
   } = useSelector(
     (state) => ({
       jobList: state.listStore.jobList || [],
+      totalCount: state.listStore.totalCount || 0,
     }),
     shallowEqual,
+  );
+
+const [filter, setFilter] = useState({
+  location: [],
+  role: [],
+  isRemote: false,
+  minExperience: 0,
+  minSalary: 0,
+});
+  const [filterOptions, setFilterOptions] = useState({ location: [], role: []});
+
+  useEffect(() => {
+    setFilterOptions({
+      location: Array.from(new Set(jobList.map(job => job.location))).filter(loc => loc !== 'remote'),
+      role: Array.from(new Set(jobList.map(job => job.jobRole))),
+      isRemote: false,
+    });
+  }, [jobList]);
+
+  const filteredJobList = jobList.filter(job => 
+    (filter.isRemote ? job.location === 'remote' : (filter.location.length === 0 || filter.location.includes(job.location))) &&
+    (filter.role.length === 0 || filter.role.includes(job.jobRole)) &&
+    (filter.minExperience === 0 || job.minExp >= filter.minExperience) &&
+    (filter.minSalary === 0 || job.minJdSalary >= filter.minSalary)
   );
 
   const loader = useRef();
@@ -38,21 +63,28 @@ function JobList() {
 
   return (
     <Container>
-      <FilterBox />
+      <FilterBox filter={filter} setFilter={setFilter} filterOptions={filterOptions} />
+      <Typography variant="h3" align="center">
+        Job Listing
+      </Typography>
+      <Typography variant="h6" align="center" gutterBottom>
+        (Total jobs: {totalCount})
+      </Typography>
       <Box>
+        
         <Grid container spacing={3}>
-          {jobList.map((job, index) => (
+          {filteredJobList.map((job, index) => (
             <Grid item xs={12} sm={6} md={4} key={job.id}>
               <JobCard job={job} />
             </Grid>
           ))}
         </Grid>
       </Box>
-    <div ref={loader} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <CircularProgress />
-    </div>
+      <div ref={loader} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <CircularProgress />
+      </div>
     </Container>
   );
-}
+};
 
 export default JobList;
